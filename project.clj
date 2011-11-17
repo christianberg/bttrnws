cf326118a333fac435073e204b324eead5c7f40d
Christian Berg <berg.christian@gmail.com>
2011-11-17 13:35:02 +0100

    Setup midje for testing.

--- project.clj
+++ project.clj
@@ -2,5 +2,7 @@
             :description "Curated Link Sharing Site"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]]
+            :dev-dependencies [[midje "1.3-alpha5"]
+                               [lein-midje "1.0.4"]]
             :main bttrnws.server)
 
