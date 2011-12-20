ce2241197fe0dbeca12c2284c5ac967c1120596c
Christian Berg <berg.christian@gmail.com>
2011-12-20 15:01:24 +0100

    Exclude clojure when fetching midje dependencies.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
             :description "Curated Link Sharing Site"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]]
-            :dev-dependencies [[midje "1.3.0"]
+            :dev-dependencies [[midje "1.3.0" :exclusions [org.clojure/clojure]]
                                [lein-midje "1.0.7"]
                                [lein-diagnostics "0.0.1"]]
             :main bttrnws.server)
