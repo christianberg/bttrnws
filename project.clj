e46c02e10d23fb9ed94fe7505319e45cdb85ff03
Christian Berg <berg.christian@gmail.com>
2011-12-20 13:25:11 +0100

    Update Midje version.

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
             :description "Curated Link Sharing Site"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]]
-            :dev-dependencies [[midje "1.3-alpha5"]
-                               [lein-midje "1.0.4"]
+            :dev-dependencies [[midje "1.3.0"]
+                               [lein-midje "1.0.7"]
                                [lein-diagnostics "0.0.1"]]
             :main bttrnws.server)
 
